c68b4dfc16897ebb172dcf753582eb5ad242439e
Charles Zhang <694556046@qq.com>
2020-07-02 21:02:22 +0800

    Finish document version control!

--- App/controllers/BaseDocumentController.py
+++ App/controllers/BaseDocumentController.py
@@ -1,4 +1,6 @@
 import os
+import json
+import shutil
 
 from elasticsearch_dsl import Search
 
@@ -10,8 +12,7 @@ from App.utils.DocumentTools import DocumentTools
 from App.utils.DocxLoader import DocxLoader
 
 
-def save_article(index_id, task_id, document_id, file, title):
-    docx_loader = DocxLoader(file)
+def save_article(index_id, task_id, document_id, docx_loader, title):
     line_list = docx_loader.text
 
     doc_id = DocumentTools.get_doc_id(index_id, task_id, document_id)
@@ -36,18 +37,24 @@ def delete_article(index_id, task_id, document_id):
         Search(index=index_id).query("match", doc_id=doc_id).delete()
 
 
-def get_article(index_id, task_id, document_id):
-    doc_id = DocumentTools.get_doc_id(index_id, task_id, document_id)
+def get_storage_path(index_id, task_id, document_id):
+    return os.path.join(get_config().DICER2_STORAGE_PATH, index_id, task_id, document_id)
 
-    doc_search = Search(index=index_id).query("match", doc_id=doc_id).sort("part")
 
-    body = []
-    for hit in doc_search.scan():
-        image_marker = 1 if hit.is_image else 0
+def get_document_storage_path(index_id, task_id, document_id, version, makedir=False):
+    storage_path = get_storage_path(index_id, task_id, document_id)
 
-        body.append((image_marker, hit.body))
+    if makedir:
+        os.makedirs(storage_path, exist_ok=True)
 
-    return body
+    file_path = os.path.join(storage_path, f"{str(version)}.dicer2doc")
+    return file_path
+
+
+def get_article_data(index_id, task_id, document_id, version):
+    file_path = get_document_storage_path(index_id, task_id, document_id, version, makedir=False)
+    fp = open(file_path, "r")
+    return json.load(fp)
 
 
 class BaseDocumentController(BaseTaskController):
@@ -57,32 +64,23 @@ class BaseDocumentController(BaseTaskController):
 
     # TODO 在删除index、task和document时，也需要删除对应的持久化文件
     # TODO 添加查询历史版本的API
-    def save_old_version(self, index_id, task_id, document_id):
-        old_document = self.get_document(index_id, task_id, document_id)
+    def persistence_version(self, index_id, task_id, document_id, docx_loader, version):
+        task_instance = self.get_task_instance(index_id, task_id)
+        doc_data = task_instance.get_doc(document_id).to_dict()
+        doc_data.update(body=docx_loader.text)
 
-        store_path = get_config().DICER2_STORAGE_PATH
-        doc_id = DocumentTools.get_doc_id(index_id, task_id, document_id)
-
-        try:
-            os.mkdir(os.path.join(store_path, doc_id))
-        except FileExistsError:
-            pass
-
-        path = os.path.join(store_path, doc_id, f"{str(old_document.version)}.dicer2doc")
-
-        # elasticsearch_dsl提供的to_dict方法只能将es field字段转化为字典返回
-        # 而body字段是一个普通属性，故需要单独添加
-        doc_data = old_document.to_dict()
-        doc_data.update(body=old_document.body)
-
-        DateEncoder.save(path, doc_data)
+        file_path = get_document_storage_path(index_id, task_id, document_id, version, makedir=True)
+        DateEncoder.save(file_path, doc_data)
 
     def create_document(self, index_id, task_id, document_id, file, **kwargs):
         task_instance = self.get_task_instance(index_id, task_id)
         task_instance.add_doc(document_id, **kwargs)
 
+        docx_loader = DocxLoader(file)
+        self.persistence_version(index_id, task_id, document_id, docx_loader, 1)
+
         title = kwargs.get("title")
-        save_article(index_id, task_id, document_id, file, title)
+        save_article(index_id, task_id, document_id, docx_loader, title)
 
         self.base.save()
 
@@ -90,6 +88,7 @@ class BaseDocumentController(BaseTaskController):
         task_instance = self.get_task_instance(index_id, task_id)
         task_instance.del_doc(document_id)
 
+        shutil.rmtree(get_storage_path(index_id, task_id, document_id))
         delete_article(index_id, task_id, document_id)
 
         self.base.save()
@@ -97,8 +96,7 @@ class BaseDocumentController(BaseTaskController):
     def update_document(self, index_id, task_id, document_id, file, **kwargs):
         task_instance = self.get_task_instance(index_id, task_id)
 
-        # 每次更新文档前先对旧版本进行持久化存储
-        self.save_old_version(index_id, task_id, document_id)
+        docx_loader = DocxLoader(file)
 
         # 如果有title等字段的更新，则先更新到dicer2_base
         task_instance.update_doc(document_id, **kwargs)
@@ -109,12 +107,14 @@ class BaseDocumentController(BaseTaskController):
 
         if file:
             delete_article(index_id, task_id, document_id)
-            save_article(index_id, task_id, document_id, file, title)
+            save_article(index_id, task_id, document_id, docx_loader, title)
+
+        self.persistence_version(index_id, task_id, document_id, docx_loader, article.version)
 
         self.base.save()
 
     def get_document(self, index_id, task_id, document_id):
         task_instance = self.get_task_instance(index_id, task_id)
         article = task_instance.get_doc(document_id)
-        article.body = get_article(index_id, task_id, document_id)
+        article.body = get_article_data(index_id, task_id, document_id, article.version).get("body")
         return article
