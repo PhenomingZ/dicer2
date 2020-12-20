3d50d3854a087eecaf45a744ddfe2fa2e225951a
phenoming <694556046@qq.com>
2020-12-20 17:41:55 +0800

    add image analyzer

--- App/utils/DocxLoader.py
+++ App/utils/DocxLoader.py
@@ -122,7 +122,7 @@ class DocxLoader(object):
 
                 result_img = "result_" + basename(part.partname)
 
-                img_ocr_text = ""
+                img_ocr_text = "文本内容："
 
                 if ocr_result.returncode:
                     base64 = gg.utils.img2base64(basename(part.partname))
@@ -130,7 +130,7 @@ class DocxLoader(object):
                     base64 = gg.utils.img2base64(result_img)
 
                     text_path = "text_" + basename(part.partname)
-                    img_ocr_text = gg.utils.readfile(text_path)
+                    img_ocr_text += "|".join(gg.utils.readfile(text_path))
 
                     os.remove(result_img)
                     os.remove(text_path)
@@ -142,5 +142,8 @@ class DocxLoader(object):
                 except OSError:
                     continue
 
-                # 添加一个元组，第一项是1代表图片，第二项是图片的hash值，第三项是图像的base64，第四项是识别出的文字
-                self._text.append((1, image_hash, base64, img_ocr_text))
+                # 添加一个元组，第一项是1代表图片，第二项是图片的hash值，第三项是图像的base64
+                self._text.append((1, image_hash, base64))
+
+                # 将从图片中识别到的文字也加入全文列表
+                self._text.append((0, img_ocr_text))
